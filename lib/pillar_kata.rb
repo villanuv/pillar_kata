83bc16e08a01afe715d682c8980a3add383e539b
Patrick Villanueva <villanuv@yahoo.com>
2018-02-23 21:28:52 -0600

    tests pass for #initialize

--- lib/pillar_kata.rb
+++ lib/pillar_kata.rb
@@ -1,5 +1,15 @@
 require "pillar_kata/version"
 
 module PillarKata
-  # Your code goes here...
+  
+  class VendingMachine
+    attr_accessor :total_deposit, :coin_return, :display
+
+    def initialize
+      @total_deposit = 0
+      @coin_return = 0
+      @display = "INSERT COIN"
+    end
+  end
+
 end
--- spec/pillar_kata_spec.rb
+++ spec/pillar_kata_spec.rb
@@ -5,3 +5,25 @@ describe PillarKata do
     expect(PillarKata::VERSION).not_to be nil
   end
 end
+
+describe PillarKata::VendingMachine do
+  before do
+    @vending_machine = PillarKata::VendingMachine.new
+  end
+
+  context "When customer approaches the machine" do
+    describe "#initialize" do
+      it "assigns 0 to @total_deposit" do
+        expect(@vending_machine.total_deposit).to eq 0
+      end
+
+      it "assigns 0 to @coin_return" do
+        expect(@vending_machine.coin_return).to eq 0
+      end
+
+      it "assigns INSERT COIN to @display" do
+        expect(@vending_machine.display).to eq "INSERT COIN"
+      end
+    end
+  end
+end
