65dc7f84227187396749d9fd0e7c30dffebf5626
Patrick Villanueva <villanuv@yahoo.com>
2018-02-24 02:32:32 -0600

    tests pass for #eval_coin... and #add_to_total...

--- lib/pillar_kata.rb
+++ lib/pillar_kata.rb
@@ -10,6 +10,26 @@ module PillarKata
       @coin_return = 0
       @display = "INSERT COIN"
     end
+
+    def evaluate_coin_by_weight_and_size(weight_in_g, diameter_in_mm)
+      if weight_in_g == 5.000 && diameter_in_mm == 21.21
+        0.05
+      elsif weight_in_g == 2.268 && diameter_in_mm == 17.91
+        0.10
+      elsif weight_in_g == 5.670 && diameter_in_mm == 24.26
+        0.25
+      elsif weight_in_g == 2.500 && diameter_in_mm == 19.05
+        0.01
+      end
+    end
+
+    def add_to_total_deposit_or_coin_return(value)
+      if value == 0.01
+        @coin_return += value
+      else
+        @total_deposit += value
+      end
+    end
   end
 
 end
--- spec/pillar_kata_spec.rb
+++ spec/pillar_kata_spec.rb
@@ -9,6 +9,10 @@ end
 describe PillarKata::VendingMachine do
   before do
     @vending_machine = PillarKata::VendingMachine.new
+    @nickel = { weight: 5.000, diameter: 21.21, value: 0.05 }
+    @dime = { weight: 2.268, diameter: 17.91, value: 0.10 }
+    @quarter = { weight: 5.670, diameter: 24.26, value: 0.25 }
+    @penny = { weight: 2.500, diameter: 19.05, value: 0.01 }
   end
 
   context "When customer approaches the machine" do
@@ -25,5 +29,53 @@ describe PillarKata::VendingMachine do
         expect(@vending_machine.display).to eq "INSERT COIN"
       end
     end
+  end
+
+  context "Accept Coins" do
+    describe "#evaluate_coin_by_weight_and_size" do
+      it "returns 0.05 for a nickel" do
+        expect(@vending_machine.evaluate_coin_by_weight_and_size(@nickel[:weight], @nickel[:diameter])).to eq 0.05
+      end
+
+      it "returns 0.10 for a dime" do
+        expect(@vending_machine.evaluate_coin_by_weight_and_size(@dime[:weight], @dime[:diameter])).to eq 0.10
+      end
+
+      it "returns 0.25 for a quarter" do
+        expect(@vending_machine.evaluate_coin_by_weight_and_size(@quarter[:weight], @quarter[:diameter])).to eq 0.25
+      end
+
+      it "returns 0.01 for a penny" do
+        expect(@vending_machine.evaluate_coin_by_weight_and_size(@penny[:weight], @penny[:diameter])).to eq 0.01
+      end
+    end
+
+    describe "#add_to_total_deposit_or_coin_return" do
+      it "adds nickels to @total_deposit" do
+        @vending_machine.add_to_total_deposit_or_coin_return(@nickel[:value])
+        expect(@vending_machine.total_deposit).to eq 0.05
+      end 
+
+      it "adds dimes to @total_deposit" do
+        @vending_machine.add_to_total_deposit_or_coin_return(@dime[:value])
+        expect(@vending_machine.total_deposit).to eq 0.10
+      end 
+
+      it "adds quarters to @total_deposit" do
+        @vending_machine.add_to_total_deposit_or_coin_return(@quarter[:value])
+        expect(@vending_machine.total_deposit).to eq 0.25
+      end
+
+      it "does NOT add pennies to @total_deposit" do
+        @vending_machine.add_to_total_deposit_or_coin_return(@penny[:value])
+        expect(@vending_machine.total_deposit).to eq 0.00
+      end
+
+      it "places pennies into @coin_return" do
+        @vending_machine.add_to_total_deposit_or_coin_return(@penny[:value])
+        expect(@vending_machine.coin_return).to eq 0.01
+      end       
+    end
+
   end
 end
