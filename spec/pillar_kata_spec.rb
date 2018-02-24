b3dc94efa8b3bf5d935b690030922e251a06140c
Patrick Villanueva <villanuv@yahoo.com>
2018-02-23 18:31:12 -0600

    added gemspec summary, rspec test is working

--- spec/pillar_kata_spec.rb
+++ spec/pillar_kata_spec.rb
@@ -4,8 +4,4 @@ describe PillarKata do
   it 'has a version number' do
     expect(PillarKata::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
