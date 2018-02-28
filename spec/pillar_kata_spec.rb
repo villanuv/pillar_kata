bc3742a5065e127445540d31d6704bee4c6a5fe8
Patrick Villanueva <villanuv@yahoo.com>
2018-02-28 02:32:03 -0600

    organized spec folder and files

--- spec/pillar_kata_spec.rb
+++ spec/pillar_kata_spec.rb
@@ -1,7 +1,1 @@
 require 'spec_helper'
-
-describe PillarKata do
-  it 'has a version number' do
-    expect(PillarKata::VERSION).not_to be nil
-  end
-end
---
+++ spec/pillar_kata_spec/version_spec.rb
@@ -0,0 +1,5 @@
+describe PillarKata do
+  it 'has a version number' do
+    expect(PillarKata::VERSION).not_to be nil
+  end
+end
