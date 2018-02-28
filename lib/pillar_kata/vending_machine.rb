6a9751b8aa5e6abdde0bdec7498ad9c8255ecee8
Patrick Villanueva <villanuv@yahoo.com>
2018-02-28 03:28:07 -0600

    changed logic for #evaluate_coin_by_weight_and_size

--- lib/pillar_kata/vending_machine.rb
+++ lib/pillar_kata/vending_machine.rb
@@ -9,15 +9,15 @@ module PillarKata
     end
 
     def evaluate_coin_by_weight_and_size(weight, diameter)
-      if weight == 5.000 && diameter == 21.21
-        0.05
-      elsif weight == 2.268 && diameter == 17.91
-        0.10
-      elsif weight == 5.670 && diameter == 24.26
-        0.25
-      elsif weight == 2.500 && diameter == 19.05
-        0.01
-      end
+      added_coin = { weight: weight, diameter: diameter }
+      nickel     = { weight: 5.000, diameter: 21.21, value: 0.05 }
+      dime       = { weight: 2.268, diameter: 17.91, value: 0.10 }
+      quarter    = { weight: 5.670, diameter: 24.26, value: 0.25 }
+      penny      = { weight: 2.500, diameter: 19.05, value: 0.01 }
+      all_coins  = [nickel, dime, quarter, penny]
+      all_coins_as_array_values = all_coins.map {|coin_object| coin_object.values}
+      id_coin_by_difference = all_coins_as_array_values.map {|coin_array| coin_array - added_coin.values}
+      id_coin_by_difference.select {|array| array.length == 1}.flatten![0]
     end
 
     def add_to_total_deposit_or_coin_return(value)
