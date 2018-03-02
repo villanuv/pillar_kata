0ac50fef23f1bc5a8cdc6a68c9b6a0ce82116776
Patrick Villanueva <villanuv@yahoo.com>
2018-03-02 03:07:49 -0600

    removed attr_accessor method testing, changed item count param value formatting

--- spec/pillar_kata_spec/vending_item_spec.rb
+++ spec/pillar_kata_spec/vending_item_spec.rb
@@ -3,24 +3,6 @@ describe PillarKata::VendingItem do
     @cola = PillarKata::VendingItem.new("cola", 1.00)
   end
 
-  context "attr_accessor methods" do
-    describe "#name" do
-      it "gets and sets @name" do
-        expect(@cola.name).to eq "cola"
-        @cola.name = "coke"
-        expect(@cola.name).to eq "coke"
-      end
-    end
-
-    describe "#price" do
-      it "gets and sets @price" do
-        expect(@cola.price).to eq 1.00
-        @cola.price = 1.25
-        expect(@cola.price).to eq 1.25
-      end
-    end
-  end
-
   describe "#initialize" do
     it "assigns name to @name" do
       expect(@cola.name).to eq "cola"
